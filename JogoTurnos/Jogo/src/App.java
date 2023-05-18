4a434a2565f9eaf2d3535a40df39c8aa43a1fba9
NevesRS <bneves.dev@gmail.com>
2023-05-17 21:33:29 -0300

    Adicionando vocações e padronizando suas vantagens e desvantagens

--- JogoTurnos/Jogo/src/App.java
+++ JogoTurnos/Jogo/src/App.java
@@ -7,6 +7,50 @@ public class App {
         Player player = new Player(10, 2, 15, 5);
         Enemy enemy = new Enemy(10, 2, 15, 5);
 
+        boolean playerIsDecided = false;
+        boolean enemyIsDecided = false;
+
+        do{
+        System.out.println(
+        "Escolha sua classe: \n Bárbaro: 15 pontos de vida, 6 de força, 8 de stamina e 5 de armadura.\n Cavaleiro: 12 pontos de vida, 3 de força, 8 de stamina e 8 de armadura. \n Ladino: 10 pontos de vida 2 de força, 15 de stamina e 5 de armadura");
+        String vocacaoPlayer = teclado.nextLine();
+        if(vocacaoPlayer.equals("barbaro")){
+            playerIsDecided = true;
+            player.setVida(25); player.setForca(8); player.setStamina(10); player.setArmadura(7);
+            // 25 + 8 + 10 + 7
+        }
+        if(vocacaoPlayer.equals("cavaleiro")){
+            playerIsDecided = true;
+            player.setVida(22); player.setForca(4); player.setStamina(12); player.setArmadura(12);
+            // 22 + 4 + 12 + 12 = 40
+        }
+        if(vocacaoPlayer.equals("ladino")){
+            playerIsDecided = true;
+            player.setVida(20); player.setForca(5); player.setStamina(20); player.setArmadura(5);
+            //20 + 5 + 20 + 5 = 40 
+        }
+        System.out.println("Vocação do player escolhida: "+vocacaoPlayer);
+        }while(playerIsDecided == false);
+        
+        do{
+            System.out.println(
+            "Escolha sua classe: \n Bárbaro: 15 pontos de vida, 6 de força, 8 de stamina e 5 de armadura.\n Cavaleiro: 12 pontos de vida, 3 de força, 8 de stamina e 8 de armadura. \n Ladino: 10 pontos de vida 2 de força, 15 de stamina e 5 de armadura");
+            String vocacaoEnemy = teclado.nextLine();
+            if(vocacaoEnemy.equals("barbaro")){
+                enemyIsDecided = true;
+                enemy.setVida(15); enemy.setForca(6); enemy.setStamina(7); enemy.setArmadura(5);
+            }
+            if(vocacaoEnemy.equals("cavaleiro")){
+                enemyIsDecided = true;
+                enemy.setVida(12); enemy.setForca(3); enemy.setStamina(8); enemy.setArmadura(8);
+            }
+            if(vocacaoEnemy.equals("ladino")){
+                enemyIsDecided = true;
+                enemy.setVida(10); enemy.setForca(2); enemy.setStamina(15); enemy.setArmadura(5);
+            }
+            System.out.println("Vocação do inimigo escolhida: "+vocacaoEnemy);
+            }while(enemyIsDecided == false);
+
         int turno = 0;
         boolean isDead = false;
 
